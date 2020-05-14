0aff00a08c6b0361f4c683c0f2b5929afc4ea513
danielkuhman <dkuhman@uab.edu>
2020-05-14 11:01:19 -0500

    Added algos for total positive and negative work at the hip

--- hip_power_vars.m
+++ hip_power_vars.m
@@ -1,5 +1,5 @@
 function [peak_ext_pos_pwr, ext_pos_work, peak_flx_neg_pwr,...
-    flx_neg_work, peak_flx_pos_pwr, flx_pos_work] = ...
+    flx_neg_work, peak_flx_pos_pwr, flx_pos_work, total_pos_work, total_neg_work] = ...
     hip_power_vars(hip_moment,hip_power,frame_rate)
 %This function finds hip joint power and mechanical work variables. 
 %The hip moment is used to identify periods of extensor and flexor activity.
@@ -8,10 +8,14 @@ function [peak_ext_pos_pwr, ext_pos_work, peak_flx_neg_pwr,...
 %Inputs: support phase hip moment and power curves, kinematic frame rate
 %Outputs: hip extensor peak positive power and positive work, hip flexor
 %peak negative power and negative work, hip flexor peak positive power and
-%positive work
+%positive work, total positive and negative work across the whole joint
 %Author: Daniel Kuhman
 %Github: https://github.com/dkuhman
 %Date created: 2020-01-24
+
+%Total positive and negative work
+total_pos_work = trapz(hip_power(hip_power > 0))/frame_rate;
+total_neg_work = trapz(hip_power(hip_power < 0))/frame_rate;
 
 %Isolate periods of extensor and flexor activity
 hip_ext_frames = find(hip_moment > 0);
